7780be4901269683d78b31a1efac44b9f7df79a0
Parthib Das <parthibdas007@gmail.com>
2025-09-03 16:20:08 +0530

    added frontend

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { ChatInterface } from '@/components/chat/ChatInterface';
+
+const Index = () => {
+  return (
+    <div className="h-screen bg-gradient-chat">
+      <ChatInterface />
+    </div>
+  );
+};
+
+export default Index;
